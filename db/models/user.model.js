ecd2572c6d96e25c4226cb97e87a312925148114
cdmejiam94 <cdmejiam94@gmail.com>
2021-10-15 00:28:43 -0500

    First Commit

---
+++ db/models/user.model.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const USER_TABLE = 'users';
+
+const UserSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+  password: {
+    allowNull: false,
+    type: DataTypes.STRING
+  }
+}
+
+class User extends Model {
+  static associate() {
+    // associate
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: USER_TABLE,
+      modelName: 'User',
+      timestamps: false
+    }
+  }
+}
+
+
+module.exports = { USER_TABLE, UserSchema, User }
